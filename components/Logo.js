b19de4d79c48ea508e7b86ec2a1f26179cc4f58a
Imran <44378535+Imranh21@users.noreply.github.com>
2021-03-26 10:28:36 +0600

    Update Logo.js

--- components/Logo.js
+++ components/Logo.js
@@ -4,6 +4,7 @@ import styles from '../styles/Logo.module.css'
 function Logo() {
     return (
         <div className={styles.logo}>
+            <img src="/images/windbnb.png" alt="windbnb"/>
             <p>Windbnb</p>
         </div>
     )
